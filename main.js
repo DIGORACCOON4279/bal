687c8753811bd197b43eea3173e75577f7e13776
DIGORACCOON4279 <dilema4000@gmail.com>
2023-10-12 16:51:05 -0500

    DarkMode

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 
 
+// Pseudocodigo
+// *crear los estilos de background y color en css
+// *crear dos constantes en el archivo de js osea main:
+//  una clase para body y otra para button  o sea la clase toggleDarkMode
+// *en la clase creada para el button toggleDarkMode escuche el evento para encender y pagar
 
-const toggleDarkMode = document.querySelector('.toggleDarkMode');
-const body = document.querySelector('body');
+// const toggleDarkMode = document.querySelector(".toggleDarkMode");
+// const body = document.querySelector("body");
 
-load();
+// toggleDarkMode.addEventListener("click", e =>{
+//     body.classList.toggle("roomDarkMode");
+// });
 
-toggleDarkMode.addEventListener('click', e =>{
-    body.classList.toggle('roomDarkMode');
-    store(body.classList.contains('roomDarkMode'));
-});
 
-function load(){
-    const roomDarkMode = localStorage.getItem('roomDarkMode');
 
-    if(!roomDarkMode){
-        store('false');
-    }
-    else if(roomDarkMode == 'true'){
-            body.classList.add('roomDarkMode');
-        }
-    }
+// const toggleDarkMode = document.querySelector('iconMoon');
+// const body = document.querySelector('body');
 
-function store(valor){
-    localStorage.setItem('roomDarkMode', valor);
+// load();
 
-}
+// toggleDarkMode.addEventListener('click', e =>{
+//     body.classList.toggle('roomDarkMode');
+//     store(body.classList.contains('roomDarkMode'));
+// });
+
+// function load(){
+//     const roomDarkMode = localStorage.getItem('roomDarkMode');
+
+//     if(!roomDarkMode){
+//         store('false');
+//     }
+//     else if(roomDarkMode == 'true'){
+//             body.classList.add('roomDarkMode');
+//         }
+//     }
+
+// function store(valor){
+//     localStorage.setItem('roomDarkMode', valor);
+
+// }
+
+
