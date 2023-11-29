056bb93cb0068eddd9119a8bd3511ffdb6be980e
DIGORACCOON4279 <dilema4000@gmail.com>
2023-11-29 13:21:32 -0500

    Improving landingPage menu

---
+++ js/place_Order.js
@@ -0,0 +1,63 @@
+
+// Place Order
+
+// Ejemplos
+
+// Tarjetas de Prueba para Stripe:
+
+// Tarjeta de Débito Exitosa:
+// Número: 4242 4242 4242 4242
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// Tarjeta de Crédito Exitosa:
+// Número: 4000 0000 0000 0002
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+// Tarjetas de Prueba para PayPal (Braintree):
+
+// Tarjeta de Débito Exitosa:
+// Número: 3782 822463 10005
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// Tarjeta de Crédito Exitosa:
+// Número: 4111 1111 1111 1111
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// *************************************************************************
+
+// Tarjetas de Débito:
+
+// MasterCard Debit:
+// Número: 5555 5555 5555 4444
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// Visa Debit:
+// Número: 4111 1111 1111 1111
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// American Express Debit:
+// Número: 3714 496353 98431
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de cuatro dígitos
+
+// Tarjetas de Crédito:
+
+// Visa:
+// Número: 4242 4242 4242 4242
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// MasterCard:
+// Número: 5555 5555 5555 4444
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de tres dígitos
+
+// American Express:
+// Número: 3782 822463 10005
+// Fecha de Vencimiento: Cualquier fecha futura
+// Código CVC: Cualquier código de cuatro dígitos
